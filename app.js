e83e91eced63f1fd691cfc2bf49ea16108b85800
darahask <darahask12@gmail.com>
2021-03-18 00:34:12 +0530

    add pr info page

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ var bodyparser = require('body-parser');
 
 app.set("view engine", "ejs");
 app.use("/", express.static('public'));
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
+app.use(bodyparser.urlencoded({extended:false}));
 
 app.get('/', function (req, res) {
     res.render("index");
 });
 
-app.post('/pullreq', function (req, res) {
-    var prurl = "";
-    res.render('prinfo.ejs', { prurl: prurl });
+var url = "";
+app.post('/prinfo', function (req, res) {
+    url = req.body.url;
+    res.send('URL Loaded');
 });
+
+app.get('/prinfo',function(req,res){
+    console.log(url);
+    res.render('prinfo',{url:url});
+})
 
 app.listen(3000, function (req, res) {
     console.log("Server Started");
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,7 +5,7 @@ function getCardHtml(url,title,body,repo){
     </div>
     <div class="card-body">
         <h5 class="card-title">${title}</h5>
-        <p class="card-text">${body}</p>
+        <p class="card-text">PR Number:${body}</p>
         <button onclick = "moreInfo('${url}')" class="btn btn-primary ">More Info</button>
     </div>
 </div>`;
@@ -13,14 +13,14 @@ function getCardHtml(url,title,body,repo){
 
 $("#pr_search").submit(function(event){
     event.preventDefault();
-    var search_url = $("#search_url").val();
+    var search_url = "https://api.github.com/repos/" + $("#search_url").val() + "/pulls";
     $.ajax({
         type:'GET',
         url:search_url,
         success: function(pr){
             var cardHtml = "";
             pr.forEach(element => {
-                cardHtml += getCardHtml(element['html_url'],element['title'],element['body'],element['base']['label'])
+                cardHtml += getCardHtml(element['url'],element['title'],element['number'],element['base']['label'])
             });
             $('#pr_list').html(cardHtml);
         }
@@ -29,5 +29,9 @@ $("#pr_search").submit(function(event){
 });
 
 function moreInfo(data){
-    console.log(data)
+    var urldata = {};
+	urldata.url = data;
+    $.post('/prinfo',urldata).done(function(data){
+        window.location.href = "/prinfo"
+    });
 }
